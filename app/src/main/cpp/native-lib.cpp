ee85186d9a6dd6052d2d8a3f40b5e8fe49ec4f4a
ModestBean <yindou97@163.com>
2018-11-04 20:30:16 +0800

    修改计算灰度值的函数

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -12,6 +12,35 @@
 char APPNAME[] = {"app"};
 using namespace cv;
 
+int graylevel(Mat image, Mat dst, Point cen, int r)//求取圆形区域内的平均灰度值
+{
+    int graysum = 0, n = 0;
+
+    for (int i = (cen.y - r); i <= (cen.y + r); ++i)//访问矩形框内的像素值
+    {
+        uchar *data = image.ptr<uchar>(i);
+        for (int j = (cen.x - r); j <= (cen.x + r); ++j) {
+            double d = (i - cen.y) * (i - cen.y) + (j - cen.x) * (j - cen.x);
+            if (d < r * r) {
+                ++n;
+                graysum += (int) data[j];
+            }
+        }
+    }
+
+    for (int i = (cen.y - r); i <= (cen.y + r); ++i)//画出圆，圆内像素值为平均灰度值
+    {
+        uchar *temp = dst.ptr<uchar>(i);
+        for (int j = (cen.x - r); j <= (cen.x + r); ++j) {
+            double d = (i - cen.y) * (i - cen.y) + (j - cen.x) * (j - cen.x);
+            if (d < r * r) {
+                temp[j] = (int) (graysum / n);
+            }
+        }
+    }
+
+    return (graysum / n);
+}
 extern "C"
 JNIEXPORT jdouble JNICALL
 Java_terry_com_greyleveltoconcentrationcamera_MainActivity_getBitmapMeanGray(JNIEnv *env,
@@ -38,8 +67,17 @@ Java_terry_com_greyleveltoconcentrationcamera_MainActivity_getBitmapMeanGray(JNI
     }
     //读取bitmap到Mat
     Mat mbgra(info.height, info.width, CV_8UC4, pixels);
+    //对图像进行圆处理
+    Mat circleImage = Mat::zeros(mbgra.size(), mbgra.type());
+    Mat mask = Mat::zeros(mbgra.size(),CV_8U);
+
+    Point circleCenter(mask.cols / 2, mask.rows / 2);
+    int radius = 600;
+
+    circle(mask, circleCenter, radius, Scalar(255),-1);
+    mbgra.copyTo(circleImage, mask);
     //复制图像
-    Mat dst = mbgra.clone();
+    Mat dst = circleImage.clone();
     Mat gray, mat_mean, mat_stddev;
     cvtColor(dst, gray, CV_RGB2GRAY); // 转换为灰度图，gray
     meanStdDev(gray, mat_mean, mat_stddev);//计算均值和标准偏差
@@ -58,11 +96,12 @@ Java_terry_com_greyleveltoconcentrationcamera_MainActivity_gray(JNIEnv *env, job
         return 0;
     }
     Mat srcImage(h, w, CV_8UC4, (unsigned char *) cbuf);
+
     Mat dst = Mat::zeros(srcImage.size(), srcImage.type());
     Mat mask = Mat::zeros(srcImage.size(),CV_8U);
-
     Point circleCenter(mask.cols / 2, mask.rows / 2);
-    int radius = min(mask.cols, mask.rows)/2;
+    //int radius = min(mask.cols, mask.rows)/2;
+    int radius = 600;
     circle(mask, circleCenter, radius, Scalar(255),-1);
     srcImage.copyTo(dst, mask);
     uchar *ptr = dst.data;
